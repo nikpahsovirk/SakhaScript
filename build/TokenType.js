13aaa8111cc5277b2a26f7a64b2ff0c7ab3dadaa
nikpahsovirk <akrivoshapkin2006@gmail.com>
2023-04-19 08:34:37 +0900

    изменил синтаксис

--- build/TokenType.js
+++ build/TokenType.js
@@ -15,9 +15,16 @@ exports.tokenTypesList = {
     'SEMICOLON': new TokenType('SEMICOLON', ';'),
     'SPACE': new TokenType('SPACE', '[ \\n\\t\\r]'),
     'ASSIGN': new TokenType('ASSIGN', 'ТЭННЭЬЭР'),
-    'LOG': new TokenType('LOG', 'СУРУЙУУ'),
+    'LOG': new TokenType('LOG', 'СУРУЙ'),
     'PLUS': new TokenType('PLUS', 'ЭБИИ'),
-    'MINUS': new TokenType('MINUS', 'КОhОРУ'),
+    'MINUS': new TokenType('MINUS', 'КО5УРЭТИИ'),
     'LPAR': new TokenType('LPAR', '\\('),
     'RPAR': new TokenType('RPAR', '\\)'),
+    'IF': new TokenType('IF', 'БАГАРДАР'),
+    'ELSE': new TokenType('ELSE', 'ОЧЧОГО'),
+    'WHILE': new TokenType('WHILE', 'ТУХАРЫ'),
+    'TRUE': new TokenType('TRUE', 'КЫРДЬЫК'),
+    'FALSE': new TokenType('FALSE', 'АЛБЫН'),
+    'MULTIPLY': new TokenType('MULTIPLY', 'ТОГУЛЛУ'),
+    'DIVISION': new TokenType('DIVISION', 'ТУНЭТИИ')
 };
